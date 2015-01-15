4ee255aca73362c4e2ae46c9737313695fa8f186
EvgenyT <evgeny.tukin@9rays.net>
2015-01-15 15:38:10 +0300

    Change notice

--- src/Pranas.WindowsTimeZoneToMomentJs/Properties/AssemblyInfo.cs
+++ src/Pranas.WindowsTimeZoneToMomentJs/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ScreenshotMonitor")]
 [assembly: AssemblyProduct("WindowsTimeZoneToMomentJs")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © ScreenshotMonitor 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
